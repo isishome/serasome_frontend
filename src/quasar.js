ef4f250f172b4191a11f3aa33d20b62adf980905
isishome <isishome@hanmail.net>
2022-07-17 16:37:26 +0900

    remove node-sass

--- src/quasar.js
+++ src/quasar.js
@@ -1,9 +1,7 @@
 import Vue from 'vue'
 
-import './styles/quasar.sass'
-import 'quasar/dist/quasar.ie.polyfills'
+import 'quasar/dist/quasar.css'
 import '@quasar/extras/material-icons/material-icons.css'
-import '@quasar/extras/fontawesome-v5/fontawesome-v5.css'
 import { Quasar, Notify, Cookies } from 'quasar'
 
 Notify.setDefaults({
