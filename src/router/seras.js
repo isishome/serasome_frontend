890cebc470d21fe9a715d9c279b88a2434f54c6c
DESKTOP-BUKLG6N\User <sjcho@morai.ai>
2022-08-29 17:58:59 +0900

    redirect

--- src/router/seras.js
+++ src/router/seras.js
@@ -82,6 +82,12 @@ const routes = [
         }
       },
       {
+        path: '9Hr_YqV7Q',
+        redirect: () => {
+          location.href = 'https://serasome.com/@Electronics/9Hr_YqV7Q'
+        }
+      },
+      {
         name: 'post-add',
         path: 'a',
         component: post
